4efcafdf5bd48f6e8c98782988769ff8bb96c826
Maël REMY--GENIN <mael.remy--genin@epfedu.fr>
2024-12-02 11:15:41 +0100

    GrilleDeJeu sans conditions de victoire

--- src/cpo_miniprojet_remy/CPO_miniprojet_REMY.java
+++ src/cpo_miniprojet_remy/CPO_miniprojet_REMY.java
@@ -4,6 +4,11 @@
  */
 package cpo_miniprojet_remy;
 
+import GrilleDeJeu.*;
+import Cellule.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Maël
@@ -15,6 +20,33 @@ public class CPO_miniprojet_REMY {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int ligne = 4;
+        int colonne = 4;
+        int bombe = 4;
+        Cellule[][] tab = new Cellule[ligne][colonne];
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                tab[i][j] = new Cellule(false, false, 0);
+            }
+        }
+
+        GrilleDeJeu tableau = new GrilleDeJeu(tab, ligne, colonne, bombe);
+        tableau.placerBombesAleatoirement();
+        System.out.println(tableau);
+
+        Scanner sc;
+        int lignec = 10;
+        int colonnec = 10;
+        while (ligne == 4) {
+            sc = new Scanner(System.in);
+            System.out.println("Choisissez une ligne");
+            lignec = sc.nextInt();
+            System.out.println("Choisissez une colonne");
+            colonnec = sc.nextInt();
+            tableau.calculerBombesAdjacentes();
+            tableau.revelerCellule(lignec, colonnec);
+            System.out.println(tableau);
+        }
     }
-    
+
 }
