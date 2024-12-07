5f47929fcf88834d41c2b56c6a4ae96b162201bb
Maël REMY--GENIN <mael.remy--genin@epfedu.fr>
2024-12-07 23:41:42 +0100

    le jeu peut fonctionner correctement. il faut juste simplifier le code avec les methodes demarrerPartie() et initialiserPartie() de la classe Partie

--- src/cpo_miniprojet_remy/CPO_miniprojet_REMY.java
+++ src/cpo_miniprojet_remy/CPO_miniprojet_REMY.java
@@ -8,6 +8,7 @@ import GrilleDeJeu.*;
 import Cellule.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import interface_graphique.*;
 
 /**
  *
@@ -20,32 +21,40 @@ public class CPO_miniprojet_REMY {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int ligne = 4;
-        int colonne = 4;
-        int bombe = 4;
-        Cellule[][] tab = new Cellule[ligne][colonne];
-        for (int i = 0; i < ligne; i++) {
-            for (int j = 0; j < colonne; j++) {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Choisissez une grille x*x");
+        int n = sc.nextInt();
+        int bombe = n;
+        Cellule[][] tab = new Cellule[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 tab[i][j] = new Cellule(false, false, 0);
             }
         }
 
-        GrilleDeJeu tableau = new GrilleDeJeu(tab, ligne, colonne, bombe);
-        tableau.placerBombesAleatoirement();
+        GrilleDeJeu grille = new GrilleDeJeu(tab, n, n, bombe);
+        Partie tableau = new Partie(grille);
+        tableau.initialiserPartie();
         System.out.println(tableau);
 
-        Scanner sc;
         int lignec = 10;
         int colonnec = 10;
-        while (ligne == 4) {
+        while (tableau.verifierVictoire()==false ^ tableau.verifierDefaite() == true) {
             sc = new Scanner(System.in);
             System.out.println("Choisissez une ligne");
             lignec = sc.nextInt();
             System.out.println("Choisissez une colonne");
             colonnec = sc.nextInt();
-            tableau.calculerBombesAdjacentes();
-            tableau.revelerCellule(lignec, colonnec);
+            tableau.tourDeJeu(lignec, colonnec);
             System.out.println(tableau);
+            if(tableau.verifierDefaite()==true){
+                System.out.println("Vous avez perdu");
+            }
+            if(tableau.verifierVictoire()==true){
+                System.out.println("Vous avez gagne");
+            }
+            
         }
     }
 
