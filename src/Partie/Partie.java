ed5ef1b42e46c25923555b15b7d870ab41eb3f78
Maël REMY--GENIN <mael.remy--genin@epfedu.fr>
2024-12-16 14:14:02 +0100

    modification pour permettre le fonctionnement du a l'ajout de l'attribut nbDrapeauAdjacent dans la classe Cellule

---
+++ src/Partie/Partie.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Partie;
+
+import Cellule.*;
+import GrilleDeJeu.*;
+import GrilleDeJeu.GrilleDeJeu;
+import GrilleDeJeu.GrilleDeJeu;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maël
+ */
+public class Partie {
+    
+    public GrilleDeJeu grille;
+    
+    public Partie(GrilleDeJeu grille) {
+        this.grille = grille;
+    }
+    
+    public GrilleDeJeu initialiserPartie() {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Choisissez une grille x*x");
+        int n = sc.nextInt();
+        double bombes = (n * n) * 0.21;
+        int bombe = (int) bombes;
+        Cellule[][] tab = new Cellule[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tab[i][j] = new Cellule(false, false, false, 0, 0);
+            }
+        }
+        GrilleDeJeu Grille = new GrilleDeJeu(tab, n, n, bombe);
+        
+        grille.placerBombesAleatoirement();
+        grille.calculerBombesAdjacentes();
+        return grille;
+    }
+    
+    public GrilleDeJeu tourDeJeu(int ligne, int colonne) {
+        if (grille.nbCasesReveles() == 0) {
+            grille.premier_coup(ligne, colonne);
+        } else {
+            grille.revelerCellule(ligne, colonne);
+        }
+        return grille;
+    }
+    
+    public boolean verifierVictoire() {
+        for (int i = 0; i < grille.getNbLines(); i++) {
+            for (int j = 0; j < grille.getNbColonnes(); j++) {
+                if (grille.getMatrice()[i][j].getdevoilee() == false && grille.getMatrice()[i][j].getPresenceBombe() == false) {
+                    return false;
+                }
+            }
+            
+        }
+        return true;
+        
+    }
+    
+    public boolean verifierDefaite() {
+        for (int i = 0; i < grille.getNbLines(); i++) {
+            for (int j = 0; j < grille.getNbColonnes(); j++) {
+                if (grille.getMatrice()[i][j].getdevoilee() == true && grille.getMatrice()[i][j].getPresenceBombe() == true) {
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
+    
+    public void demarrerPartie(Partie grille) {
+        while (verifierVictoire() == false) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Choisissez une ligne");
+            int ligne = sc.nextInt();
+            System.out.println("Choisissez une colonne");
+            int colonne = sc.nextInt();
+            tourDeJeu(ligne, colonne);
+            System.out.println(grille);
+            
+        }
+        
+    }
+    
+    @Override
+    public String toString() {
+        String texte = " ";
+        for (int i = 0; i < grille.getNbColonnes(); i++) {
+            texte += " " + String.valueOf(i + 1);
+        }
+        for (int i = 0; i < grille.getNbLines(); i++) {
+            texte += "\n" + String.valueOf(i + 1);
+            for (int j = 0; j < grille.getNbColonnes(); j++) {
+                texte += " " + grille.getMatrice()[i][j].toString(grille.getMatrice()[i][j].getdevoilee(), grille.getMatrice()[i][j].getNbBombesAdjacentes(), grille.getMatrice()[i][j].getPresenceBombe());
+            }
+        }
+        return texte;
+    }
+    
+}
